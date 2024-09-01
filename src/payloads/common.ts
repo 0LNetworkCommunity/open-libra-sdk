9efe7bbffc61c0ae615309dd204e554643b2b3d7
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-09-01 23:01:39 +0200

    names

--- src/payloads/common.ts
+++ src/payloads/common.ts
@@ -1,6 +1,6 @@
 import type { ViewObj } from './types'
 
-export const account_balance_payload = (address: string): ViewObj => ({
+export const accountBalancePayload = (address: string): ViewObj => ({
   function: '0x1::ol_account::balance',
   type_arguments: [],
   arguments: [address],
--- src/payloads/system.ts
+++ src/payloads/system.ts
@@ -1,24 +1,18 @@
 import type { ViewObj } from './types'
 
-export const fees_collected_payload: ViewObj = {
+export const feesCollectedPayload: ViewObj = {
   function: '0x1::transaction_fee::system_fees_collected',
   type_arguments: [],
   arguments: [],
 }
 
-export const epoch_length_payload: ViewObj = {
+export const epochLengthPayload: ViewObj = {
   function: '0x1::block::get_epoch_interval_secs',
   type_arguments: [],
   arguments: [],
 }
 
-export const vdf_difficulty: ViewObj = {
-  function: '0x1::tower_state::get_difficulty',
-  type_arguments: [],
-  arguments: [],
-}
-
-export const infra_balance: ViewObj = {
+export const infraBalance: ViewObj = {
   function: '0x1::infra_escrow::infra_escrow_balance',
   type_arguments: [],
   arguments: [],
