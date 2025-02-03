c8bb5e40b4f7ef3e2256b3637f984687c7a0bf4b
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-02-03 12:18:48 -0500

    patch lint

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  // TODO: adjust config for src and test dirs in TS, and dist/ for minified release
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
   { languageOptions: { globals: globals.browser } },
