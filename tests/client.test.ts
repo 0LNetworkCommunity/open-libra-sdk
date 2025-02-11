45e881cb2d81df88f31c8c4b75fbe6051cb5b7e5
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-02-10 21:37:10 -0500

    update image

--- tests/client.test.ts
+++ tests/client.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from "bun:test";
 import { LibraClient } from "../src/api/api";
 import { currentValidatorsPayload } from "../src/payloads/validators";
+import { accountBalancePayload } from "../src/payloads/common";
 
 test("can get validators", async () => {
   console.log("Calling Libra Explorer API");
@@ -20,4 +21,19 @@ test("can get validators", async () => {
   } else {
     throw new Error("Expected p[0] to be an array");
   }
+});
+
+test("can get balance", async () => {
+  console.log("Calling Libra Explorer API");
+  const client = new LibraClient();
+  await client.connect();
+  client.assertReady();
+
+  const res = await client.getIndex();
+
+  // testnet is 2
+  expect(res.chain_id).toBe(2);
+  let b = accountBalancePayload("0x87515d94a244235a1433d7117bc0cb154c613c2f4b1e67ca8d98a542ee3f59f5");
+  const p = await client.postViewFunc(b);
+  console.log(p)
 });
