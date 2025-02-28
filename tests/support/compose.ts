b75b13a2118147b66bd85b6a7c999e7af8f94bb5
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-02-28 16:32:32 -0300

    [containers] check containers are running before test (#7)
    * readme
    
    * compose.ts throws on container start error
    
    * patch compose entrypoint

--- tests/support/compose.ts
+++ tests/support/compose.ts
@@ -1,6 +1,7 @@
 import {
   downAll,
   logs,
+  ps,
   upAll,
   type IDockerComposeOptions,
 } from "docker-compose";
@@ -14,25 +15,59 @@ const options: IDockerComposeOptions = {
   // log: true,
 };
 
-export async function testnetUp(): Promise<void> {
+export async function testnetUp(): Promise<boolean> {
   await upAll(options);
 
-  const targetString = "QCReady"; // Replace with the actual string you expect
-
+  const targetString = "QCReady";
+  console.log("waiting for logs");
   while (true) {
     const logOutput = await logs("alice", options);
-    //       const targetString = `"event":"NewRound","reason":"QCReady"`; // Replace with the actual string you expect
 
     if (logOutput.out.includes(targetString)) {
       console.log("testnet started, proceeding...");
       break; // Exit the loop when the message is found
     }
 
+    if (logOutput.out.includes("exited with code")) {
+      console.log(`last logs: ${logOutput.out}`);
+      throw new Error("containers exited with non zero code!");
+    }
+
+    const runs = await isComposeRunning(composeFilePath);
+    if (!runs) {
+      console.log(`last logs: ${logOutput.out}`);
+
+      throw new Error("containers are not running!");
+    }
     // check logs every second
     await new Promise((resolve) => setTimeout(resolve, 1_000));
   }
+  return true;
 }
 
 export async function testnetDown() {
   await downAll(options);
+}
+
+async function isComposeRunning(path: string): Promise<boolean> {
+  try {
+    const result = await ps({
+      cwd: path,
+      commandOptions: [["--format", "json"]],
+    });
+    if (!result || result.data.services.length == 0) {
+      console.error("[ERROR] no containers running");
+      return false;
+    }
+    for (const service of result.data.services) {
+      if (service.state != "running") {
+        console.error(`[ERROR] service ${service.name} is not running`);
+        return false;
+      }
+    }
+  } catch {
+    console.error("[ERROR] could not run docker-compose ps");
+    return false;
+  }
+  return true;
 }
