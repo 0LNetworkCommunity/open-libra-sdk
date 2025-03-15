9b30b22b15152db2afd58b39cf9200322fa8b981
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-03-15 15:12:49 -0400

    export testnet

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from "./api/api.ts";
 export * from "./crypto/keyFactory.ts";
 export * from "./wallet/libraWallet.ts";
+export * from "./local_testnet/compose.ts";
+export * from "./local_testnet/fixture_mnemonics.ts";
 // Export all types from vendor
 export * from "@aptos-labs/ts-sdk";
