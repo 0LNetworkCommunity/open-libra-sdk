20a76059401297d883e12791ba8d0db265c3a265
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-03-15 15:26:46 -0400

    update node vs browser targets

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export * from "./api/api.ts";
-export * from "./crypto/keyFactory.ts";
-export * from "./wallet/libraWallet.ts";
-export * from "./local_testnet/compose.ts";
-export * from "./local_testnet/fixture_mnemonics.ts";
-// Export all types from vendor
-export * from "@aptos-labs/ts-sdk";
+
+// Re-export browser compatible modules
+export * from "./indexBrowser.ts";
+
+// Node.js specific exports
+export * from "./local_testnet/compose";
+export * from "./local_testnet/fixture_mnemonics";
